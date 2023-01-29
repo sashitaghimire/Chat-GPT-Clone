bc5cefecdf366c96a09c2040e1fe0e7d73ae683b
sashitagh <sashita@varicon.com.au>
2023-01-29 18:03:53 +0545

    create a input field and button

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,21 @@ function App() {
   const openai = new OpenAIApi(configuration);
   const generateImage = async () => {
     const res = await openai.createImage({
-      prompt: "Say this is a test",
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
     });
-    console.log(res.data.data[0]?.url);
   };
 
   return (
-    <div>
-      <button onClick={generateImage}>Generate an imahge</button>
+    <div className="app-main">
+      <h3>Generate Image using Open AI</h3>
+      <input
+        className="app-input"
+        placeholder="Type something to Generate an image"
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button onClick={generateImage}>Generate an image</button>
     </div>
   );
 }
